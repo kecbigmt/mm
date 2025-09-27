cde9d231883f95b9b8bbacd5ed33f9377846004a
kecy <keishiro.oyama@kecy.me>
2025-09-27 20:49:44 +0900

    refactor(repositories): expose listByPath lookup

--- src/domain/repositories/item_repository.ts
+++ src/domain/repositories/item_repository.ts
@@ -1,6 +1,6 @@
 import { Result } from "../../shared/result.ts";
 import { Item } from "../models/item.ts";
-import { PlacementBin } from "../models/placement.ts";
+import { Path } from "../primitives/mod.ts";
 import { ItemId } from "../primitives/mod.ts";
 import { ItemShortId } from "../primitives/item_short_id.ts";
 import { RepositoryError } from "./repository_error.ts";
@@ -10,8 +10,8 @@ export interface ItemRepository {
   load(id: ItemId): Promise<Result<Item | undefined, RepositoryError>>;
   save(item: Item): Promise<Result<void, RepositoryError>>;
   delete(id: ItemId): Promise<Result<void, RepositoryError>>;
-  listByPlacementBin(
-    bin: PlacementBin,
+  listByPath(
+    path: Path,
   ): Promise<Result<ReadonlyArray<Item>, RepositoryError>>;
   findByShortId(
     shortId: ItemShortId,
